7a014625e99c1b6399aabe28f594eea59779b3c2
hazardstef <stefton2013@mail.ru>
2025-03-28 15:41:59 +0300

    Добавьте файлы проекта.

---
+++ Homework 1.1/Homework 1.1.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::getline(std::cin, name);
+
+    std::cout << name <<"\n";
+    std::cout << name.length() << "\n";
+    std::cout << name.front() << " " << name.back();
+
+    std::cin;
+    return 0;
+
+}
+
